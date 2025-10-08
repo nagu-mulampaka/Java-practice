12090cbb11fd16b119caada82f4f52917095396f
Nagu Mulampaka <nagumulampaka@gmail.com>
2025-10-08 19:58:18 +0530

    Add files via upload

---
+++ FirstProgram.java
@@ -0,0 +1,7 @@
+// This is My first Java Program
+class FirstProgram {
+    public static void main(String args[])
+    {
+        System.out.println("Hello Everyone! I'm Nagu Mulampaka, and today I wrote my first Java program!");
+    }
+}
